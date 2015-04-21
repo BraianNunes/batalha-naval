dd30bdc9e87e65f9df512bbad5eac4ea5f23cfa0
Braian Nunes <braiannunes@live.com>
2015-04-21 18:45:02 -0300

    Configuracao do porta aviões.

---
+++ src/main/java/br/iesb/batalhanaval/model/Embarcacao.java
@@ -0,0 +1,12 @@
+package br.iesb.batalhanaval.model;
+import java.util.List;
+/**
+ * Created by Braian Nunes on 4/21/2015.
+ */
+public interface Embarcacao {
+    String getName();
+
+    int getLife();
+
+    List<Location> getLocations();
+}
---
+++ src/main/java/br/iesb/batalhanaval/model/PortaAvioes.java
@@ -0,0 +1,45 @@
+package br.iesb.batalhanaval.model;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Braian Nunes on 4/21/2015.
+ */
+public class PortaAvioes implements Embarcacao {
+    private static final int LIFE = 5;
+
+    @Override
+    public String getName() {
+        return "Porta aviões!";
+    }
+
+    @Override
+    public int getLife() {
+        return LIFE;
+    }
+
+    @Override
+    public List<Location> getLocations() {
+        List<Location> locations = new ArrayList<Location>();
+
+        Location l1 = new Location(0, 0);
+        locations.add(l1);
+
+        Location l2 = new Location(0, 1);
+        locations.add(l2);
+
+        Location l3 = new Location(0, 2);
+        locations.add(l3);
+
+        Location l4 = new Location(1, 1);
+        locations.add(l4);
+
+        Location l5 = new Location(2, 1);
+        locations.add(l5);
+
+        if (locations.size() != LIFE) {
+            throw new IllegalArgumentException("Quantidade de localizações incorretas para o " + this.getName());
+        }
+
+        return locations;
+    }
+}
